16511390b122bced4a801348cc612ec807fd63b5
Mihir <mihirkantho@gmail.com>
2022-06-16 16:48:25 +0600

    Final without responsive

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,9 +1,41 @@
 import React from 'react'
 import './Contact.css'
+import { MdEmail } from 'react-icons/md'
+import { BsWhatsapp } from 'react-icons/bs'
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+        <h5>Get In Touch</h5>
+        <h2>Contact Me</h2>
+
+        <div className="container contact-container">
+              <div className="contact-options">
+                <article className='contact-option'>
+                  <MdEmail/>
+                  <h4>Email</h4>
+                  <h5>mihirkantho@gmail.com</h5>
+                  <a href="mailto:mihirkantho@gmail.com" target='_blank' rel="noreferrer">Send a message</a>
+                </article>
+
+                <article className='contact-option'>
+                  <BsWhatsapp/>
+                  <h4>WhatsApp</h4>
+                  <h5>+8801740623262</h5>
+                  <a href="https://wa.me/+8801740623262" target='_blank' rel="noreferrer">Ping me on Whats app</a>
+                </article>
+
+              </div>
+              {/* End of the Contact OPtion  */}
+
+              <from action="">
+                <input type="text" name='name' placeholder='Your Full Name' />
+                <input type="email" name='email' placeholder='Your valid Email' />
+                <textarea name="message" id="" rows="7" placeholder='Type your Message'></textarea>
+                <button type='submit' className='btn btn-primary'>Send Message</button>
+              </from>
+      </div>
+    </section>
   )
 }
 
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,10 +1,36 @@
 import React from 'react'
 import './Footer.css'
+import { BsFacebook } from 'react-icons/bs'
 
-function Footer() {
-  return (
-    <div>Footer</div>
+
+const Footer = () =>{
+  return(
+    <footer>
+      <a href="#" className='footer-logo'>Mihir</a>
+
+      <ul className='weblink'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#service">Service</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer-social">
+        <a href="https://facebook.com"><BsFacebook/></a>
+        <a href="https://facebook.com"><BsFacebook/></a>
+        <a href="https://facebook.com"><BsFacebook/></a>
+      </div>
+
+      <div className="footer-copyright">
+        <small>&copy;Mihir Das All right reserved.</small>
+      </div>
+    </footer>
+
   )
 }
+
 
 export default Footer
